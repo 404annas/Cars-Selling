a516d9cc35cdec3f114f5d4fdc25610f3a84ebca
Muhammad Annas Khan <annasking601@gmail.com>
2026-02-26 17:04:35 +0500

    GA & GSC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ import Footer from "@/components/Footer";
 import Bot from "@/components/Bot";
 import Footer2 from "@/components/Footer2";
 
+import Script from "next/script";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -43,6 +45,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script type="text/javascript" id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vne0fvwfbc");
+          `}
+        </Script>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WF0T8DDP07"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WF0T8DDP07');
+          `}
+        </Script>
+      </head>
       <body
         className={`${poppins.variable} ${orbitron.variable} antialiased`}
       >
@@ -53,7 +80,7 @@ export default function RootLayout({
         <Footer2 />
 
         <Bot />
-        
+
         <Toaster
           position="bottom-right"
           richColors={true}
