1eee4308a37d691f1f920f3a5aabb94a8bf77639
Muhammad Annas Khan <annasking601@gmail.com>
2026-03-29 01:17:43 +0500

    pictures order update for id=0

--- src/app/blogs/BlogsContent.tsx
+++ src/app/blogs/BlogsContent.tsx
@@ -68,6 +68,7 @@ export default function BlogsContent() {
                             </p>
                         </div>
                     </div>
+                    
                 </section>
 
                 {/* Cover Image Section */}
